3a4922c89cafaca7e96c19e1181db4ea7f40029e
Muditha Sasanka Kodikara <121278918+mudithasasanka7@users.noreply.github.com>
2025-06-09 08:29:11 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,7 +28,7 @@ function App() {
         {/* Header */}
         <header className="mb-8">
           <h1 className="text-3xl font-bold text-gray-800">
-            Developer Analytics Dashboard
+            Developer Analytics Dashboard testing
           </h1>
         </header>
 
